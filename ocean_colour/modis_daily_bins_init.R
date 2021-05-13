4787dfdc14d30326071608d5a2d1c884f27ee0e6
Michael Sumner <mdsumner@gmail.com>
2021-05-13 02:34:50 +0000

    remove parallel code and make robust to new dirs

--- ocean_colour/modis_daily_bins_init.R
+++ ocean_colour/modis_daily_bins_init.R
@@ -6,26 +6,20 @@ library(dplyr)
 files <- ocfiles(time.resolution = "daily", product = "MODISA", varname = "RRS", type = "L3b", bz2.rm = TRUE, ext = "nc") %>%
   as_tibble() %>% transmute(date, file = basename(fullname), fullname)
 
-#daterange <- as.POSIXct(c("2015-07-01", "2016-07-01"), tz = "GMT")
 lonrange <- c(-180, 180)
-#latrange <- c(-78, -30)
 latrange <- c(-90, 90)
 
 ## initialize the bin logic for MODISA
 init <- initbin(NUMROWS = 4320)
 latbin_idx <- which(between(init$latbin, latrange[1], latrange[2]))
 bins <- tibble(bin_num = seq(init$basebin[min(latbin_idx)], init$basebin[min(c(max(latbin_idx)+1, length(init$latbin)))]))
-# xy <- bin2lonlat(bins$bin_num, 4320)
-# bins <- bins %>% filter(between(xy$x, lonrange[1], lonrange[2])) %>%
-#   mutate(bin_idx = row_number())
-# rm(xy)
 
 outp <- "/rdsi/PRIVATE/raad/data_local/acecrc.org.au/ocean_colour/modis_daily"
 get_l3 <- function(file_package, outpath) {
   file <- file_package$file
   datei <- file_package$datei
   yr <- format(datei, "%Y")
-  
+
 #  "/rdsi/PRIVATE/raad/data_local/acecrc.org.au/ocean_colour/modis_daily",
   fname <- file.path(outpath, yr, sprintf("%s", format(datei, "modis_%Y%j.rds")))
  if (file.exists(fname)) return(NULL)
@@ -45,12 +39,21 @@ get_l3 <- function(file_package, outpath) {
   saveRDS(d1, fname, compress = FALSE)
 }
 
-pkgs <- lapply(seq(nrow(files)), function(x) list(file = files$fullname[x], datei = files$date[x], bins = bins))
-library(future.apply)
-plan(multiprocess)
 
-#"2018-04-03 16:12:15 AEST"
-print(Sys.time())
-aa <- future_lapply(pkgs, get_l3, outpath = outp)
-print(Sys.time())
+for (i in seq_len(nrow(files))) {
 
+  datei = files$date[i]
+  yr <- format(datei, "%Y")
+
+  if (!file.exists(file.path(outp, yr))) {
+    dir.create(file.path(outpath, yr))
+  }
+
+  file <- files$fullname[i]
+  fname <- file.path(outp, yr, sprintf("%s", format(datei, "modis_%Y%j.rds")))
+  if (!file.exists(fname)) {
+
+    pkg <- list(file = file, datei = datei, bins = bins)
+    get_l3(pkg, outpt)
+  }
+}
